02eb1dfae249aca2d47e933f460ca4941bc7be95
vitaliy <zndev@ya.ru>
2023-03-19 16:31:08 +0600

    upd

--- src/Libs/TemplateRender.php
+++ src/Libs/TemplateRender.php
@@ -2,16 +2,23 @@
 
 namespace Untek\Core\Text\Libs;
 
+use Untek\Core\Text\Helpers\TemplateHelper;
+
 class TemplateRender
 {
 
     protected $replacement = [];
     protected string $beginBlock = '{{';
     protected string $endBlock = '}}';
-
-
-    public function addReplacement(string $placeholderName, string $value): void
+    
+    public function renderTemplate(string $template): string
+    {
+        return TemplateHelper::render($template, $this->replacement, $this->beginBlock, $this->endBlock);
+    }
+    
+    public function addReplacement(string $placeholderName, string $value): self
     {
         $this->replacement[$placeholderName] = $value;
+        return $this;
     }
 }
